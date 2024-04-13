34b5689d623ad52d02c4b3a91593adcc2b215a5e
Abdillah <Abdillahissa866@gmail.com>
2024-04-13 03:10:40 +0300

    get the data from the server and display to home screen

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:todo_app/add_page.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,6 +12,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List items = [];
+  @override
+  void initState() {
+    super.initState();
+    fetchTodo();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,6 +26,18 @@ class _HomePageState extends State<HomePage> {
         title: const Text('Todo'),
         centerTitle: true,
       ),
+      body: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items[index];
+            return ListTile(
+              leading: CircleAvatar(
+                child: Text('${index + 1}'),
+              ),
+              title: Text(item['title']),
+              subtitle: Text(item['description']),
+            );
+          }),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: navigateToAddPage,
         label: const Text("Add Todo"),
@@ -31,5 +53,20 @@ class _HomePageState extends State<HomePage> {
       context,
       route,
     );
+  }
+
+  Future<void> fetchTodo() async {
+    var url = "https://api.nstack.in/v1/todos?page=1&limit=10";
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    if (response.statusCode == 200) {
+      final jsonData = jsonDecode(response.body) as Map;
+      final result = jsonData['items'] as List;
+      setState(() {
+        items = result;
+      });
+    } else {
+      //show error message for fail response
+    }
   }
 }
